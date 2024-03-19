e07ef21f1205ce205e9abf6c50e072ca6b3a8c95
Hashan <hashan.chanaka96@gmail.com>
2024-03-19 20:32:05 +0000

    updated

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 "use client";
 import { Counter } from "./Counter";
+import DataFetcher from "./DataFetcher";
 
 export default function Home() {
   return (
     <>
       <Counter />
+      <DataFetcher />
     </>
   );
 }
--- app/store.js
+++ app/store.js
@@ -2,10 +2,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./counterSlice";
 import counter2Slice from "./counter2Slice";
+import dataReducer from "./dataSlice";
 
 export const store = configureStore({
   reducer: {
     counter1: counterSlice,
     counter2: counter2Slice,
+    data: dataReducer,
   },
 });
